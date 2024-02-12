0669f06af6e64a90932378257b68f3bbc479f271
sitiamindd <105641379+sitiamindd@users.noreply.github.com>
2024-02-12 15:14:52 +0800

    Update sum_template.cpp

--- sum_template.cpp
+++ sum_template.cpp
@@ -1,7 +1,7 @@
 //reference code to use template in cpp
+//create sum and substraction
 
 #include<iostream>
-#include <vector>
 
 using namespace std;
 
@@ -16,6 +16,17 @@ Summable sum(const Summable data[], int size, Summable s=0)
     return s;
 }
 
+template<class Substraction>
+Substraction substract(const Substraction data[], int size, Substraction s=0)
+{
+    for(int i=0; i<size; ++i)
+    {
+        s -= data[i];
+    }
+    
+    return s;
+}
+
 int main(void)
 {
     cout << "template for sum()" << endl;
@@ -24,6 +35,8 @@ int main(void)
     
     cout << sum(a,3) << endl;
     cout << sum(b,3) << endl;
+    cout << substract(a,3) << endl;
+    cout << substract(b,3) << endl;
     
     
     
